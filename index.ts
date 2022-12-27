8c0967243fa0a2ed7acc62c79d0d0e807ccb67b5
nemusona <nemusonaneko@protonmail.com>
2022-12-27 11:14:52 -0600

    arcade adapter

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { getDataArcade } from "./adapters/arcade";
 import { getDataBendDao } from "./adapters/benddao";
 import { getDataJpegd } from "./adapters/jpegd";
 import { getDataLlamalend } from "./adapters/llamalend";
@@ -5,17 +6,18 @@ import { getDataNftFi } from "./adapters/nftfi";
 import { getDataX2y2 } from "./adapters/x2y2";
 
 async function run() {
-  const nft = "0x2c889A24AF0d0eC6337DB8fEB589fa6368491146";
-  console.log("Bend DAO");
-  console.log(await getDataBendDao(nft));
-  console.log("JPEGd");
-  console.log(await getDataJpegd(nft));
-  console.log("LlamaLend");
-  console.log(await getDataLlamalend(nft));
-  console.log("NFTFi");
-  console.log(await getDataNftFi(nft));
-  console.log("X2Y2");
-  console.log(await getDataX2y2(nft));
+  const nft = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d";
+  // console.log("Bend DAO");
+  // console.log(await getDataBendDao(nft));
+  // console.log("JPEGd");
+  // console.log(await getDataJpegd(nft));
+  // console.log("LlamaLend");
+  // console.log(await getDataLlamalend(nft));
+  // console.log("NFTFi");
+  // console.log(await getDataNftFi(nft));
+  // console.log("X2Y2");
+  // console.log(await getDataX2y2(nft));
+  await getDataArcade(nft);
 }
 
 run();
