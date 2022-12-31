3f3a8b13ef38dbd5b3b13f6375ab2c3b18310ba1
nemusona <nemusonaneko@protonmail.com>
2022-12-30 20:09:58 -0600

    x2y2

--- adapters/x2y2/index.ts
+++ adapters/x2y2/index.ts
@@ -1,49 +1,31 @@
-import { gql, request } from "graphql-request";
-import { SECONDS_IN_A_WEEK } from "../../utils/constants";
-const api = "https://api.thegraph.com/subgraphs/name/nemusonaneko/x2y2-borrows";
-
-async function subgraphCall(nft: string) {
-  const now = Math.floor(Date.now() / 1e3);
-  const weekAgo = now - SECONDS_IN_A_WEEK;
-  const query = gql`
-    query {
-        nfts(where: { id: "${nft.toLowerCase()}" }) {
-            loans(where: {blockTimestamp_gte: "${weekAgo}", blockTimestamp_lte: "${now}"}) {
-                adminShare
-                blockNumber
-                blockTimestamp
-                borrowAmount
-                borrower
-                collateralId
-                id
-                lender
-                loanDuration
-                loanId
-                loanStart
-                repayAmount
-                token
-                txHash
-            }
-        }
-    }
-    `;
-  return (await request(api, query)).nfts;
-}
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export async function getDataX2y2(nft: string) {
-  const subgraphCallResult = await subgraphCall(nft);
-  if (subgraphCallResult.length === 0) return [];
-  const loans = subgraphCallResult[0].loans;
-  const results = [];
-  for (let i = 0; i < loans.length; i++) {
-    const loan = loans[i];
-    results.push({
-      token: loan.token,
-      borrowAmount: loan.borrowAmount,
-      repayAmount: loan.repayAmount,
-      loanDuration: loan.loanDuration,
-      timestamp: loan.blockTimestamp,
-    });
-  }
-  return results;
+  const now = Math.floor(Date.now() / 1e3);
+  const res = await fetch(
+    `https://loan-api.x2y2.org/v1/offer/list?nftAddress=${nft.toLowerCase()}&tokenId=0`,
+    {
+      headers: {
+        "x-api-key": process.env.X2Y2_API_KEY,
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res.data.list);
+  const result = [];
+  res.forEach((res) => {
+    if (res.expireTime >= now) {
+      result.push({
+        amount: res.amount,
+        repayment: res.repayment,
+        apr: res.apr,
+        expires: res.expireTime,
+        adminFee: res.adminFee,
+        duration: res.duration,
+        token: res.erc20Address,
+      });
+    }
+  });
+  return result;
 }
